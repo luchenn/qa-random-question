6f4f6d88ec41c4a9003c5595e31dbab776e32cb5
NoahY <noahy57@gmail.com>
2011-09-05 18:24:52 +0530

    streamline

--- qa-random-question.php
+++ qa-random-question.php
@@ -1,57 +1,46 @@
 <?php
 
-	class qa_random_questions {
+    class qa_random_questions {
 
-		function allow_template($template)
-		{
-			$allow=false;
-			
-			switch ($template)
-			{
-				case 'activity':
-				case 'qa':
-				case 'questions':
-				case 'hot':
-				case 'ask':
-				case 'categories':
-				case 'question':
-				case 'tag':
-				case 'tags':
-				case 'unanswered':
-				case 'user':
-				case 'users':
-				case 'search':
-				case 'admin':
-					$allow=true;
-					break;
-			}
-			
-			return $allow;
-		}
+        function allow_template($template)
+        {
 
-		function allow_region($region)
-		{
-			return ($region=='side');
-		}
+            $allow=true;
+            
+            return $allow;
+        }
 
-		function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
-		{
-            $random_question = qa_db_read_one_assoc(
+        function allow_region($region)
+        {
+            return ($region=='side');
+        }
+
+        function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
+        {
+            $rand = qa_db_read_one_value(
                 qa_db_query_sub(
-                    'SELECT * FROM ^posts WHERE type=$ ORDER BY rand() LIMIT 1',
-                    'Q'
+                    'SELECT MAX(postid) FROM ^posts'
                 ),
                 true
             );
-
-			if (is_array($random_question)) {
-				$themeobject->output('<hr/><h2>Random Question</h2>');
-				$themeobject->output('<p style="margin:0 0 10px 0; font-weight:bold;"><a href="'.qa_path_html(qa_q_request($random_question['postid'], $random_question['title'])).'">'.$random_question['title'].'</a></p><p>'.($random_question['netvotes']>0?'+'.$random_question['netvotes'].' vote, ':(((int)$random_question['netvotes'])<0?$random_question['netvotes'].' vote, ':'')).$random_question['acount'].' answer'.($random_question['acount']==1?'':'s').'</p><hr/>');
-			}
-		}
-	};
+            if(!$rand) return;
+            $rand = rand()/getrandmax()*$rand;
+            $random_question = qa_db_read_one_assoc(
+                qa_db_query_sub(
+                    'SELECT * FROM ^posts WHERE type=$ AND postid >= # LIMIT 1',
+                    'Q',$rand
+                ),
+                true
+            );
+            
+            if (is_array($random_question)) {
+                $themeobject->output('<hr/><h2>Random Question</h2>');
+                $themeobject->output('<p style="margin:0 0 10px 0; font-weight:bold;"><a href="'.qa_path_html(qa_q_request($random_question['postid'], $random_question['title'])).'">'.$random_question['title'].'</a></p><p>'.($random_question['netvotes']>0?'+'.$random_question['netvotes'].' vote, ':(((int)$random_question['netvotes'])<0?$random_question['netvotes'].' vote, ':'')).$random_question['acount'].' answer'.($random_question['acount']==1?'':'s').'</p><hr/>');
+            }
+        }
+    };
 
 
 /*
-	Omit PHP closing tag to help avoid accidental output
+    Omit PHP closing tag to help avoid accidental output
 */
