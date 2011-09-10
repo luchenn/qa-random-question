2b33e6191ce47e586bc382c0671ac32aab26b7c7
NoahY <noahy57@gmail.com>
2011-09-10 21:50:09 +0530

    translatable

--- qa-random-question.php
+++ qa-random-question.php
@@ -17,30 +17,44 @@
 
         function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
         {
-            $max = qa_db_read_one_value(
-                qa_db_query_sub(
-                    'SELECT MAX(postid) FROM ^posts'
-                ),
-                true
-            );
-            
+            $temp = qa_db_query_sub(
+				'SELECT BINARY title as title, postid, netvotes, acount FROM ^posts WHERE type=$',
+                'Q'
+			);
+            $max = mysql_num_rows($temp);
             if(!$max) return;
             
             $themeobject->output('<h2>'.qa_opt('random_question_title').'</h2>');
 
             for($i=0;$i < qa_opt('random_question_number'); $i++) {
-            
+                
                 $rand = rand()/getrandmax()*$max;
-                $random_question = qa_db_read_one_assoc(
-                    qa_db_query_sub(
-                        'SELECT BINARY title as title, postid, netvotes, acount FROM ^posts WHERE type=$ AND postid >= # LIMIT 1',
-                        'Q',$rand
-                    ),
-                    true
+                $check = mysql_data_seek(
+                    $temp,$rand
                 );
+                if(!check) $i--;
+                else $random_question = qa_db_read_one_assoc($temp,true);
                 
                 if (is_array($random_question)) {
-                    $themeobject->output('<div class="random-question" style="padding-bottom:4px;"><a href="'.qa_path_html(qa_q_request($random_question['postid'], $random_question['title'])).'">'.$random_question['title'].'</a></div><div class="random-question-meta" style="padding-bottom:4px; border-bottom:1px solid; margin-bottom:2px;">'.($random_question['netvotes']>0?'+'.$random_question['netvotes'].' vote, ':(((int)$random_question['netvotes'])<0?$random_question['netvotes'].' vote, ':'')).$random_question['acount'].' answer'.($random_question['acount']==1?'':'s').'</div>');
+                    $votes = (int)$random_question['netvotes'];
+                    if($votes>0) {
+                        if($votes > 1) $votes = str_replace('^','+'.$votes,qa_lang('main/x_votes'));
+                        else $votes = '+'.qa_lang('main/1_vote');
+                    }
+                    else if($votes<0) {
+                        if($votes < -1) $votes = str_replace('^',$votes,qa_lang('main/x_votes'));
+                        else $votes = '-'.qa_lang('main/1_vote');
+                    }
+                    else $votes = '';
+                    
+                    $answers = (int)$random_question['acount'];
+                    if($answers>0) {
+                        if($answers > 1) $answers = str_replace('^','+'.$answers,qa_lang('main/x_answers'));
+                        else $answers = qa_lang('main/1_answer');
+                    }
+                    else $answers = '';
+                    
+                    $themeobject->output('<div class="random-question" style="padding-bottom:4px;"><a href="'.qa_path_html(qa_q_request($random_question['postid'], $random_question['title'])).'">'.$random_question['title'].'</a></div><div class="random-question-meta" style="padding-bottom:4px; border-bottom:1px solid; margin-bottom:2px;">'.$votes.($votes&&$answers?', ':'').$answers.'</div>');
                 }
             }
         }
